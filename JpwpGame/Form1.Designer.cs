51f79ae96df1ff37bb6340e665dfe4677d66cc72
JSkubij <117028445+JSkubij@users.noreply.github.com>
2022-10-30 16:47:45 +0100

    Add files via upload

---
+++ JpwpGame/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace JpwpGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tytul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tytul
+            // 
+            this.tytul.AutoSize = true;
+            this.tytul.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tytul.Location = new System.Drawing.Point(465, 72);
+            this.tytul.Name = "tytul";
+            this.tytul.Size = new System.Drawing.Size(255, 33);
+            this.tytul.TabIndex = 0;
+            this.tytul.Text = "Placeholder tytułu ";
+            this.tytul.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 985);
+            this.Controls.Add(this.tytul);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tytul;
+    }
+}
+
